2a74893408595f2636e6e5a77631f9ca4c9209a2
Gurgen <mkrtchyangurgen8@mail.ru>
2022-01-30 16:57:15 +0400

    portfolio by Gurgen Mkrtchyan

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://www.mindtree.com/sites/default/files/2020-10/increasing-change-velocity-american-services-company-case-study%5B1%5D_0.jpg",
+    link: "https://github.com/Gurgen8",
+  },
+  {
+    id: 2,
+    img: "https://s3.amazonaws.com/startupcollective-com/wp-content/uploads/digital-675x320.jpg",
+    link: "https://github.com/Gurgen8",
+  },
+  {
+    id: 3,
+    img: "https://north-live.s3.eu-west-2.amazonaws.com/wp-content/uploads/2019/06/29141453/Identifying-Cyber-Threats-Risks-Masthead.png",
+    link: "https://github.com/Gurgen8",
+  },
+  {
+    id: 4,
+    img: "https://tekzens.com/wp-content/uploads/2020/06/DevOps-use.jpg",
+    link: "https://github.com/Gurgen8",
+  },
+  {
+    id: 5,
+    img: "https://www.openaccessgovernment.org/wp-content/uploads/2020/10/dreamstime_l_195954831.jpg",
+    link: "https://github.com/Gurgen8",
+  },
+  {
+    id: 6,
+    img: "https://sis.com.hk/wp-content/uploads/2019/04/distribution-1000x600.jpg",
+    link: "https://github.com/Gurgen8",
+  },
+];
